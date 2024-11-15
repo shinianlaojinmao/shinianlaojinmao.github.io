3709e97ce3d844df31b98bf4d6ce38ce9ad23634
daming-Z <daming17@163.com>
2024-11-15 19:36:06 +0800

    $

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/10/20/hello-world/","2024/10/27/hello/","2024/10/27/666/","2024/10/27/hello-world - 副本 - 副本/","2024/10/27/hello-world - 副本/","2024/11/09/今天/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
