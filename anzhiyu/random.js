5f9f77a5e8adbd6503a3b9f1aecbc2a9c94f9694
daming-Z <daming17@163.com>
2024-12-10 21:51:04 +0800

    $

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/23/etahzdngxfmsjau5yWEGDxfh,viug9tfsaye/","2024/10/27/hello-world - 副本 - 副本/","2024/10/20/hello-world/","2024/10/27/hello-world - 副本/","2024/10/27/hello/","2024/11/30/hexo网站/","2024/11/23/jmg/","2024/11/09/今天/","2024/11/16/无语大事件/","2024/10/27/666/","2024/11/16/那些winxp里的一些东西（怀旧）/","2024/11/29/华为手机助手---1/"];function toRandomPost(){
+var posts=["2024/10/27/666/","2024/11/23/etahzdngxfmsjau5yWEGDxfh,viug9tfsaye/","2024/10/27/hello-world - 副本 - 副本/","2024/10/27/hello-world - 副本/","2024/10/20/hello-world/","2024/10/27/hello/","2024/11/30/hexo网站/","2024/11/23/jmg/","2024/11/09/今天/","2024/11/16/无语大事件/","2024/11/29/华为手机助手---1/","2024/11/16/那些winxp里的一些东西（怀旧）/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
